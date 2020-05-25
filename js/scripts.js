d06848fcf158329848df754f01b21ccea5267d98
Екатерина Лукинова <katyushta@rambler.ru>
2020-05-25 19:44:58 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,112 @@
+
+$( document ).ready(function() {
+
+    if (screen.width < 768) {
+        $('.carousel').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            arrows: false,
+            slidesToShow: 1.2,
+            slidesToScroll: 1,
+        });
+    }
+
+    if (screen.width < 990) {
+        $('.team-carousel').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 1.1
+                    }
+                }
+            ]
+        });
+
+        $(".team-button-prev").click(function () {
+            $('.team-carousel').slick('slickPrev');
+        });
+        $(".team-button-next").click(function () {
+            $('.team-carousel').slick('slickNext');
+        });
+    }
+
+    $('.post-carousel').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1.2
+                }
+            },
+            {
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1.2
+                }
+            }
+        ]
+    });
+
+    $(".post-button-prev").click(function () {
+        $('.post-carousel').slick('slickPrev');
+    });
+    $(".post-button-next").click(function () {
+        $('.post-carousel').slick('slickNext');
+    });
+
+    $('.news-img-carousel').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1.1,
+                    dots:false
+                }
+            }
+        ]
+    });
+
+    $(".news-button-prev").click(function () {
+        $('.news-img-carousel').slick('slickPrev');
+    });
+    $(".news-button-next").click(function () {
+        $('.news-img-carousel').slick('slickNext');
+    });
+
+    $('.faq-card-header').click(function() {
+        var isActive = $(this).hasClass("active");
+        $('div.faq-card-header').removeClass('active');
+        if (isActive) {
+            $(this).removeClass('active')
+        } else {
+            $(this).addClass("active");
+        }
+
+    });
+});
